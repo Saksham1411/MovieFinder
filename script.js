9a80eeefe508bfd6dd9ba11d91b9a18a2b0bb7f7
Saksham1411 <sakshamsamgarg1411@gmail.com>
2023-10-05 16:48:01 +0530

    remove button

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let search = document.getElementById('search');
 let btn = document.getElementById('btn');
 let showFav = document.getElementById('showFav');
 
-window.addEventListener('DOMContentLoaded',()=>{
+window.addEventListener('DOMContentLoaded', () => {
     let fav = JSON.parse(localStorage.getItem('fav'));
-    addToDisplay(fav);
+    addToDisplay(fav, 'rem');
 })
 
-showFav.addEventListener('click',()=>{
+showFav.addEventListener('click', () => {
     let fav = JSON.parse(localStorage.getItem('fav'));
-    addToDisplay(fav);
+    addToDisplay(fav, 'rem');
 })
 
 let ExtractedData;
-btn.addEventListener('click', async()=>{
-    ExtractedData= await getdata();
+btn.addEventListener('click', async () => {
+    ExtractedData = await getdata();
 });
 
 // console.log('hello');
@@ -24,56 +24,70 @@ async function getdata() {
     let data = await raw.json();
 
     let arr = data.Search;
-    search.value="";
+    search.value = "";
 
-    addToDisplay(arr);
+    addToDisplay(arr, 'add');
     return arr;
 }
 
-function addToDisplay(arr) {
+function addToDisplay(arr, type) {
     output = '';
     console.log(arr);
     if (arr === null) {
         document.getElementById('main').innerHTML = "no movie found";
     } else {
-        arr.forEach((i,idx) => {
+        arr.forEach((i, idx) => {
             output += `<div class="card">
             <div>
                 <img src='${i.Poster}' alt='Poster'>
                 <p>Name:- ${i.Title}</p>
                 <p>Type:- ${i.Type}</p>
-            </div>
-            <div class=hide>
-                <button class='btn' id='fav' key='${idx}'>Add to Favorite</button>
-            </div>
-            </div>
-            `;
+            </div>`
+            if (type === 'add') {
+                output += `
+                    <div class=hide>
+                        <button class='btn' id='fav' key='${idx}'>Add to Favorite</button>
+                    </div>
+                </div>`
+            }
+            if(type === 'rem'){
+                output += `
+                    <div class=hide>
+                        <button class='btn' id='rem'>Remove</button>
+                    </div>
+                </div>`
+            }
         });
         document.getElementById('main').innerHTML = output;
         initFav();
     }
 }
 
-function initFav(){
+function initFav() {
     let favBtn = document.querySelectorAll('#fav');
-    favBtn.forEach(i=>{
-        i.addEventListener('click',(e)=>{
+    favBtn.forEach(i => {
+        i.addEventListener('click', (e) => {
             let idx = e.target.getAttribute('key');
             addToFav(ExtractedData[idx]);
         })
     });
 }
 
-function addToFav(item){
+function addToFav(item) {
     let fav;
-    if(localStorage.getItem('fav')==null){
-        fav=[];
-        localStorage.setItem('fav',JSON.stringify(fav));
-    } else{
+    if (localStorage.getItem('fav') == null) {
+        fav = [];
+        localStorage.setItem('fav', JSON.stringify(fav));
+    } else {
         fav = JSON.parse(localStorage.getItem('fav'));
     }
-    
+
     //adding
-    fav.push(item);
-    localStorage.setItem('fav',JSON.stringify(fav));
+    console.log(item);
+    let flag = false;
+    fav.some(i => {
+        if (i.imdbID === item.imdbID) flag = true;
+    })
+    if (!flag) fav.push(item);
+    localStorage.setItem('fav', JSON.stringify(fav));
 }
